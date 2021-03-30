1c1dd565e6b5833a333c87019c5dcba1dafff402
Owais <precisework01@gmail.com>
2021-03-30 23:58:01 +0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Box } from "@material-ui/core"
+import {Basket} from './components/Basket';
+import {Product} from './components/Product';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth='md'>
+      <Box mt={5} mb={5}>
+        <Product />
+      </Box>
+      <Box mt={5} mb={5}>
+       <Basket />
+       </Box>
+      </Container>
     </div>
   );
 }
---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+export interface ProductItem {
+
+    id: string
+    title:string
+    description:string
+    price: number
+    imageUrl: string
+    added ?: boolean
+}
